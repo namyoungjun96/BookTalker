9c629991a27e0cf29eae1e133adea02cadebc2c6
namyoungjun96 <ehdgornltls@naver.com>
2025-08-19 22:41:08 +0900

    book_talker_backend_first commit

---
+++ book_talker_backend/src/main/java/com/example/book_talker_backend/BookTalkerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_talker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTalkerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookTalkerBackendApplication.class, args);
+    }
+
+}
---
+++ book_talker_backend/src/test/java/com/example/book_talker_backend/BookTalkerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_talker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTalkerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
