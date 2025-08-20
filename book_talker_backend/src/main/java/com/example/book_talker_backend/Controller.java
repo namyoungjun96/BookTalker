5e80ea71b66f67b4f41a681ccecef9b97111470f
nam-yeongjun <fuswns96@mobigen.com>
2025-08-20 17:19:28 +0900

    로그 레벨 변경, redis가 실제 세션과 연동되는 것 확인

--- book_talker_backend/src/main/java/com/example/book_talker_backend/Controller.java
+++ book_talker_backend/src/main/java/com/example/book_talker_backend/Controller.java
@@ -1,12 +1,31 @@
 package com.example.book_talker_backend;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class Controller {
+
     @GetMapping
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping(value = "/save/{name}")
+    public String save(HttpServletRequest request, @PathVariable String name) {
+        HttpSession session = request.getSession();
+        session.setAttribute("name", name);
+        return "success";
+    }
+
+    @GetMapping(value = "/session-info")
+    public String sessionInfo(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        return session.getAttribute("name").toString();
     }
 }
