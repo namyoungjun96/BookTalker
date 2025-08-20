306bfba461f0c7ac9f0120745c39d432dea3697b
nam-yeongjun <fuswns96@mobigen.com>
2025-08-20 16:21:52 +0900

    security 추가, 인증 비활성화 하고 spring redis + session 동작 확인

--- book_talker_backend/src/main/java/com/example/book_talker_backend/config/SecurityConfig.java
+++ book_talker_backend/src/main/java/com/example/book_talker_backend/config/SecurityConfig.java
@@ -1,4 +1,23 @@
 package com.example.book_talker_backend.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authorizeRequests ->
+                authorizeRequests.anyRequest().permitAll())
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+        ;
+
+        return http.build();
+    }
 }
