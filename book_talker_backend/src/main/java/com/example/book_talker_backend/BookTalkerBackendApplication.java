509b6977d408a5f91ca9daa62fe76541fb652de4
nam-yeongjun <ehdgornltls@naver.com>
2025-10-06 19:30:09 +0900

    jpa 활성화

--- book_talker_backend/src/main/java/com/example/book_talker_backend/BookTalkerBackendApplication.java
+++ book_talker_backend/src/main/java/com/example/book_talker_backend/BookTalkerBackendApplication.java
@@ -2,8 +2,10 @@ package com.example.book_talker_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class BookTalkerBackendApplication {
 
     public static void main(String[] args) {
