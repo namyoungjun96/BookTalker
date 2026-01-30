41d9c96f11c6d3624d222bd8c46a5a4d89866e08
namyoungjun96 <ehdgornltls@naver.com>
2026-01-30 18:58:53 +0900

    FEAT: 글로벌 익셉션 핸들러 뼈대만 생성

---
+++ book_talker_backend/src/main/java/com/example/book_talker_backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.example.book_talker_backend.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler {
+}
